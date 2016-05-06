2200d51b2f0604e12a4f202e16c8d0c77f12f42f
thadino <pwntfire@hotmail.com>
2016-05-06 17:44:50 +0200

    Should provide Sucessfull JSON code to send to Kasper (the send part is not implemented yet though)

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -14,6 +14,108 @@ app.controller('View1Ctrl', function ($scope, $http)
 {
 
 
+  $("#bookingPage").hide();
+
+if(AirlineURL !== null) // so it does not reset the value of AirlineURL
+var AirlineURL;
+
+if(FlightID !== null) // so it does not reset the value of flightID
+var FlightID;
+
+if(numberofseats !== null) // so it does not reset the value of flightID
+var numberofseats;
+
+if(reserveePhone !== null) // so it does not reset the value of flightID
+var reserveePhone;
+
+if(reserveeName !== null) // so it does not reset the value of flightID
+var reserveeName;
+
+if(reserveeEmail !== null) // so it does not reset the value of flightID
+var reserveeEmail;
+
+
+$scope.switchPage = function(url, id) {
+    AirlineURL = url;
+    FlightID = id;
+    
+    
+    
+    
+    
+    
+    
+
+    
+    $("#result").hide();
+    $("#bookingPage").show();
+};
+
+$scope.bookvacation = function()
+{
+  
+  //  alert("Airline URL: " + AirlineURL + " Flight ID: " + FlightID);
+    
+
+    reserveePhone = $("#phone").val();
+    reserveeName = $("#name").val();
+    reserveeEmail = $("#email").val();
+    numberofseats = $("#member").val();
+    
+    var times = -1;
+ $('.myawesomefuckingclass').each(function(i, obj) {
+     
+     times++;
+     
+    
+});  
+
+var myString = ""; 
+
+ $('.myawesomefuckingclass').each(function(i, obj) {
+
+     if(i%2 === 0)
+     myString += '{"firstName":"'+obj.value+'",';
+ 
+     if(i%2 === 1 && i !== times )
+     myString += '"lastName":"'+obj.value +'"},';
+ 
+     if(i === times)
+     {
+      myString += '"lastName":"'+obj.value +'"}';  
+     }
+ 
+    
+     
+    
+});
+
+var JSONtoSend = "";
+ JSONtoSend = '{ "url" : "'+ AirlineURL +'",'+
+ '"flightID":"'+ FlightID + '",' +
+ '"numberOfSeats":"'+numberofseats+'",' +
+ '"reserveeName":"'+reserveeName+'",' +
+ '"reserveePhone":"'+reserveePhone+'",' +
+ '"reserveeEmail":"'+reserveeEmail+'",' +
+ '"passengers" :[' +
+ myString +
+ ']}';
+ 
+//  '{"firstName":"'+FlightID+'","lastName":"'+FlightID+'"},' +
+// '{"firstName":"'+FlightID+'","lastName":"'+FlightID+'"},' +
+//  '{"firstName":"'+FlightID+'","lastName":"'+FlightID+'"}' +
+ console.log(JSONtoSend);
+
+   
+
+
+
+
+};
+
+
+
+
 
 
 
@@ -72,24 +174,6 @@ app.controller('View1Ctrl', function ($scope, $http)
 
     };
 
-//    $scope.start = function(){
-//        $scope.startBooking();
-//    };
-//    
-//    $scope.startBooking = function(){
-//            var number = document.getElementById("member").value;
-//            var container = document.getElementById("container");
-//            while (container.hasChildNodes()) {
-//                container.removeChild(container.lastChild);
-//            }
-//            for (i=0;i<number;i++){
-//                container.appendChild(document.createTextNode("Member " + (i+1)));
-//                var input = document.createElement("input");
-//                input.type = "text";
-//                container.appendChild(input);
-//                container.appendChild(document.createElement("br"));
-//            }
-//        };
 
     $scope.makeBooking = function (fID) {
 
@@ -125,31 +209,36 @@ app.filter('myFilter', function () {
 
 function addFields() {
     var number = document.getElementById("member").value;
+
     var container = document.getElementById("passengers");
-    alert(document.getElementById("passengers"));
+
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
     }
-    for (i = 0; i < number; i++) {
-        container.appendChild(document.createTextNode("Passenger " + (i + 1) + " "));
+    for (var i = 0; i < number; i++) {
+        container.appendChild(document.createTextNode("Passenger"));
         var input = document.createElement("input");
+        var input2 = document.createElement("input");
         input.type = "text";
-        container.appendChild(input);
+        input.placeholder = "firstname";
+        input.style = "margin-left:15px;";
+        input.className = "myawesomefuckingclass";
+        
+                container.appendChild(input);
+        
+        input2.type = "text";
+        input2.placeholder = "lastname";
+        input2.style = "margin-left:15px;";
+        input2.className = "myawesomefuckingclass";
+
+        container.appendChild(input2);
         container.appendChild(document.createElement("br"));
         container.appendChild(document.createElement("br"));
     }
 }
-;
 
 
 
-
-function switchPage() {
-
-    $("#result").hide();
-    $("#bookingPage").show();
-}
-;
 
 
 
